049b86c59033aeac57e801c8d4743340d24c13cb
Jakaria <jakariahossen2025@gmail.com>
2026-07-05 07:48:39 +0600

    updated

--- js/firebase.js
+++ js/firebase.js
@@ -7,22 +7,21 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebas
 // Authentication
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
-// Firebase Configuration
 const firebaseConfig = {
+  
   apiKey: "AIzaSyD9NoaOrtABI63unHmcDHMpK6Kwv4KfqHI",
   authDomain: "organic-food-jakaria.firebaseapp.com",
   projectId: "organic-food-jakaria",
   storageBucket: "organic-food-jakaria.firebasestorage.app",
   messagingSenderId: "283724929228",
   appId: "1:283724929228:web:9693d5a5bcc7d41a1d4a69"
+  
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Services
 const db = getFirestore(app);
+
 const auth = getAuth(app);
 
-// Export services
-export { app, db, auth };
+export { db, auth };
