82038f855cced781de9f3888169a0123193f0084
Spck editor <jakariahossen2025@gmail.com>
2026-07-14 21:47:45 +0600

    update

--- js/checkout.js
+++ js/checkout.js
@@ -48,26 +48,7 @@ window.placeOrder = async function() {
   }
   
   try {
-    function generateOrderId() {
-  
-  const now = new Date();
-  
-  const date =
-    now.getFullYear().toString() +
-    String(now.getMonth() + 1).padStart(2, "0") +
-    String(now.getDate()).padStart(2, "0");
-  
-  const time =
-    String(now.getHours()).padStart(2, "0") +
-    String(now.getMinutes()).padStart(2, "0") +
-    String(now.getSeconds()).padStart(2, "0");
-  
-  const ms =
-    String(now.getMilliseconds()).padStart(3, "0");
-  
-  return `OF-${date}-${time}-${ms}`;
-  
-}
+    
 const orderId = generateOrderId();
     await addDoc(collection(db, "orders"), {
       orderId: orderId,
@@ -87,13 +68,9 @@ const orderId = generateOrderId();
       
     });
     
-    localStorage.removeItem("cart");
-    
     document.getElementById("successOrderId").textContent = orderId;
 
 document.getElementById("successModal").style.display = "flex";
-    
-    window.location.href = "index.html";
     
   }
   
@@ -104,5 +81,50 @@ document.getElementById("successModal").style.display = "flex";
     console.log(error);
     
   }
+  
+}
+// Copy Order ID
+document.getElementById("copyBtn").onclick = function() {
+  
+  const id = document.getElementById("successOrderId").textContent;
+  
+  navigator.clipboard.writeText(id);
+  
+  this.textContent = "✅ Copied";
+  
+  setTimeout(() => {
+    
+    this.textContent = "📋";
+    
+  }, 2000);
+  
+};
+
+// Back to Home
+document.getElementById("homeBtn").onclick = function() {
+  
+  localStorage.removeItem("cart");
+  
+  window.location.href = "index.html";
+  
+};
+function generateOrderId() {
+  
+  const now = new Date();
+  
+  const date =
+    now.getFullYear().toString() +
+    String(now.getMonth() + 1).padStart(2, "0") +
+    String(now.getDate()).padStart(2, "0");
+  
+  const time =
+    String(now.getHours()).padStart(2, "0") +
+    String(now.getMinutes()).padStart(2, "0") +
+    String(now.getSeconds()).padStart(2, "0");
+  
+  const ms =
+    String(now.getMilliseconds()).padStart(3, "0");
+  
+  return `OF-${date}-${time}-${ms}`;
   
 }
